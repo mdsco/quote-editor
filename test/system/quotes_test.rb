c92638ae23ae58a90ba8a20fb7be733ce83794be
mike <mike@healpay.com>
2025-03-01 19:22:40 -0500

    model, controller, views

---
+++ test/system/quotes_test.rb
@@ -0,0 +1,57 @@
+require "application_system_test_case"
+
+class QuotesTest < ApplicationSystemTestCase
+  setup do
+    @quote = quotes(:first)
+  end
+
+  test "Creating a new quote" do
+    # When we visit the quotes index page
+    # we expect to see a title with the text "Quotes"
+    visit quotes_path
+    assert_selector "h1", text: "Quotes"
+
+    # When we click on New Quote we expect to 
+    # land on a page with the title "New Quote"
+    click_on "New Quote"
+    assert_selector "h1", text: "New Quote"
+
+    # When we fill in the quote text and click "Create Quote"
+    fill_in "Name", with: "Capybara quote"
+    click_on "Create quote"
+
+    # we expect to be directed to the index page and
+    # see a new quote item in the page list
+    assert_selector "h1", text: "Quotes"
+    assert_text "Capybara quote"
+  end
+
+  test "Showing a quote" do
+    visit quotes_path
+
+    click_link @quote.name
+    assert_selector "h1", text: @quote.name
+  end
+
+  test "Updating a quote" do
+    visit quotes_path
+    assert_selector "h1", text: "Quotes"
+
+    click_on "Edit", match: :first
+    assert_selector "h1", text: "Edit Quote"
+
+    fill_in "Name", with: "Updated Quote"
+    click_on "Update quote"
+
+    assert_selector "h1", text: "Quotes"
+    assert_text "Updated Quote"
+  end
+
+  test "Destroying a quote" do
+    visit quotes_path
+    assert_text @quote.name
+
+    click_on "Delete", match: :first
+    assert_no_text @quote.name
+  end
+end
