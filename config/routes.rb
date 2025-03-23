86f76454e74610ca62cec6f6a52554810669cbaa
mike <mike@healpay.com>
2025-03-23 15:33:15 -0400

    add line item date crud

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root "pages#home"
   
   devise_for :users
-  resources :quotes
+  resources :quotes do
+    resources :line_item_dates, except: [:index, :show]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
